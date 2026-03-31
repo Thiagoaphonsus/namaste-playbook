802aea1d678879f5a08b9e2bc335dc5ba3942da5
Thiagoaphonsus <thiago@namasteagency.com.br>
2026-03-31 13:30:53 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PlaybookHub from './pages/Index.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PlaybookHub />
+  </React.StrictMode>,
+)
